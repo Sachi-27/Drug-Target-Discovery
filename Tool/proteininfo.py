0893995ce1fd84465b0b102e85570befad8fecf4
Sachi-27 <sabya.liku@gmail.com>
2024-09-09 14:19:10 +0530

    pub counts

--- Tool/proteininfo.py
+++ Tool/proteininfo.py
@@ -8,28 +8,26 @@ with open("protein_names.json") as f:
 
 
 # returns a list of pubmed ids in reverse order of publication date
-def get_protein_info(uniprot_id, source, search_for="uniprot_id", retrieve_amount=30):
+def get_protein_info(uniprot_id, source, retrieve_amount=30, mindate=0, maxdate=2024):
     if source == "pubmed":
-        if search_for == "uniprot_id":
-            # Fetching XML data from the URL
-            url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax={retrieve_amount}&sort=pub+date&term={uniprot_id}"
-        elif search_for == "druggability_info":
-            if uniprot_id not in protein_names:
-                # This case is not handled in this version -- these proteins have been moved from uniprot to uniparc
-                return 0, []
-            elif "shortNames" not in protein_names[uniprot_id]:
-                protein_name = protein_names[uniprot_id]["fullName"]
-                search_term = f'(({uniprot_id}[All Fields]) OR ({protein_name}[All Fields])) AND (druggability[All Fields] OR "drug target"[All Fields] OR inhibitor[All Fields] OR "protein target"[All Fields] OR "drug discovery"[All Fields] OR "drug binding"[All Fields] OR "drug interaction"[All Fields] OR "targeted therapy"[All Fields])'
-            else:
-                protein_name = protein_names[uniprot_id]["fullName"]
-                alias_names = protein_names[uniprot_id]["shortNames"]
-                search_term = f'(({uniprot_id}[All Fields]) OR ({protein_name}[All Fields]) '
-                for alias in alias_names:
-                    search_term += f'OR ({alias}[All Fields]) '
-                search_term += f') AND (druggability[All Fields] OR "drug target"[All Fields] OR inhibitor[All Fields] OR "protein target"[All Fields] OR "drug discovery"[All Fields] OR "drug binding"[All Fields] OR "drug interaction"[All Fields] OR "targeted therapy"[All Fields])'
-            encoded_search_term = requests.utils.quote(search_term)
-            url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax={retrieve_amount}&sort=pub+date&term={encoded_search_term}"
+        if uniprot_id not in protein_names:
+            # This case is not handled in this version -- these proteins have been moved from uniprot to uniparc
+            return 0, []
+        elif "shortNames" not in protein_names[uniprot_id]:
+            protein_name = protein_names[uniprot_id]["fullName"]
+            search_term = f'(("{uniprot_id}"[Title/Abstract] OR "{protein_name}"[Title/Abstract]) AND ((druggability[Title/Abstract] OR "drug target"[Title/Abstract] OR "protein target"[Title/Abstract] OR "drug discovery"[Title/Abstract] OR "drug binding"[Title/Abstract] OR "drug interaction"[Title/Abstract] OR "targeted therapy"[Title/Abstract]) OR ("cancer"[Title/Abstract] OR "tumor"[Title/Abstract] OR "neurodegenerative"[Title/Abstract] OR "disorders"[Title/Abstract] OR "metabolic disorders"[Title/Abstract] OR "cardiovascular"[Title/Abstract] OR "COPD"[Title/Abstract] OR "infectious"[Title/Abstract] OR "disease"[Title/Abstract])))'
+        else:
+            protein_name = protein_names[uniprot_id]["fullName"]
+            alias_names = protein_names[uniprot_id]["shortNames"]
+            search_term = f'(({uniprot_id}[Title/Abstract] OR "{protein_name}"[Title/Abstract])'
+            for alias in alias_names:
+                search_term += f' OR "{alias}"[Title/Abstract]'
+            search_term += f') AND ((druggability[Title/Abstract] OR "drug target"[Title/Abstract] OR "protein target"[Title/Abstract] OR "drug discovery"[Title/Abstract] OR "drug binding"[Title/Abstract] OR "drug interaction"[Title/Abstract] OR "targeted therapy"[Title/Abstract]) OR ("cancer"[Title/Abstract] OR "tumor"[Title/Abstract] OR "neurodegenerative"[Title/Abstract] OR "disorders"[Title/Abstract] OR "metabolic disorders"[Title/Abstract] OR "cardiovascular"[Title/Abstract] OR "COPD"[Title/Abstract] OR "infectious"[Title/Abstract] OR "disease"[Title/Abstract]))'
 
+        search_term += " AND Humans[MeSH Terms]"
+        # print(search_term)  
+        encoded_search_term = requests.utils.quote(search_term)
+        url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax={retrieve_amount}&mindate={mindate}&maxdate={maxdate}&sort=pub+date&term={encoded_search_term}"
         response = requests.get(url)
         xml_data = response.text
         root = ET.fromstring(xml_data)
@@ -45,10 +43,32 @@ def get_protein_info(uniprot_id, source, search_for="uniprot_id", retrieve_amoun
     else:
         raise Exception(f"Do not support source {source}")
 
+def get_pmid_info(pmid):
+    url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id={pmid}&retmode=xml"
+    response = requests.get(url)
+    tree = ET.fromstring(response.content)
+    title = tree.findtext(".//Item[@Name='Title']")
+    pub_year = tree.findtext(".//Item[@Name='PubDate']")
+    authors = [author.text for author in tree.findall(".//Item[@Name='Author']")]
+    doi = tree.findtext(".//Item[@Name='DOI']")
+    link_from_doi = f"https://doi.org/{doi}"
+    return {
+        "title": title,
+        "pub_year": pub_year,
+        "authors": authors,
+        "doi": doi,
+        "link": link_from_doi
+    }
+
 if __name__ == '__main__':
     uniprot_id = 'P05067'
     source = 'pubmed'
-    count, pubmed_ids = get_protein_info(uniprot_id, source, search_for="uniprot_id", retrieve_amount=30)
+    count, pubmed_ids = get_protein_info(uniprot_id, source, retrieve_amount=10, mindate=2014)
     print(count, pubmed_ids)
-    count, pubmed_ids = get_protein_info(uniprot_id, source, search_for="druggability_info", retrieve_amount=30)
-    print(count, pubmed_ids)
+
+    pmid_details = {}
+    for pmid in pubmed_ids:
+        pmid_details[pmid] = get_pmid_info(pmid)
+        print(pmid, pmid_details[pmid])
+
+
